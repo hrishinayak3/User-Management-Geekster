0c6d1e513f90ebb0258edf00fb3f2f7feddb0441
Hrishikesh <hrishinayak3@gmail.com>
2023-10-19 04:09:28 +0530

    first commit

---
+++ src/test/java/com/geekster/ecommerce/mvc/EcommerceMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geekster.ecommerce.mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
